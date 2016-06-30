cbcc6a761a1a6ae4a90917bc748732ff1f46ff59
zoolsher <zoolsher@gmail.com>
2016-06-30 10:53:24 +0800

    formart code

--- server-models/user/index.js
+++ server-models/user/index.js
@@ -1,34 +1,34 @@
 var md5 = require('crypto-js/md5');
 
 function User(db) {
-    function passwordHash(pass){
+    function passwordHash(pass) {
         return md5(pass);
     }
-    function checkpass(pass,hash){
-        return md5(pass) === hash; 
+    function checkpass(pass, hash) {
+        return md5(pass) === hash;
     }
     function login(name, pass, callback) {
         var res = db.collection('users').find({
             name: name
         });
         var userArr = [];
-        res.each(function(err,doc){
-            if(err) throw err;
-            if(doc!=null){
-                if(checkpass(pass,doc.pass)){
+        res.each(function (err, doc) {
+            if (err) throw err;
+            if (doc != null) {
+                if (checkpass(pass, doc.pass)) {
                     userArr.push(doc);
                 }
-            }else{
-                if(userArr.length===0){
-                    callback(null,false);
-                }else{
-                    callback(null,userArr[0]);
-                }    
+            } else {
+                if (userArr.length === 0) {
+                    callback(null, false);
+                } else {
+                    callback(null, userArr[0]);
+                }
             }
         })
     }
     return {
-        login:login
+        login: login
     }
 }
 
